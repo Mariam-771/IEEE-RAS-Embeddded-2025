82de50293bdb84c93b9030d8c6d9a8481cccf077
Mariam-771 <mariamadel4910@gmai.com>
2025-03-24 16:33:14 +0200

    first

---
+++ Task1/Problem5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+//https://codeforces.com/submissions/anonymous78#
+int main()
+{
+ int n, m;
+  while(1){
+ int min, max, sum=0;
+
+  scanf("%d %d", &n, &m);
+
+  if(m<=0 || n<=0)
+    break;
+
+  if(m>n){min=n; max=m;}
+  else{min=m; max=n;}
+
+  for(int i=min ; i<=max ; i++){
+  printf("%d ", i);
+  sum += i;
+  }
+  printf("sum =%d\n", sum);
+  }
+    return 0;
+}
---
+++ Task1/Problem7.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int isPrime(int num){
+
+    if(num == 2){
+        return 1;
+    }
+    if(num % 2 == 0 || num < 2 ){
+            return 0;
+    }
+
+    for(int i = 3 ; i <= sqrt(num) ; i+=2){
+       if(num % i == 0) return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+ int num, sum=0;
+
+ scanf("%d", &num);
+
+ while(num > 0){
+ sum += num % 10;
+ num /= 10;
+ }
+
+ if(isPrime(sum)){
+printf("The sum of digits (%d) is a prime number.", sum);
+    }
+else{
+printf("The sum of digits (%d) is not a prime number.", sum);
+}
+
+    return 0;
+}
